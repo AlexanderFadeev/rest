4e51d665f29cf636d9799a9b83efc70ddba14713
AlexanderFadeev <1alexking1@gmail.com>
2019-07-25 20:02:57 +0300

    Add reply impl

--- reply.go
+++ reply.go
@@ -1,8 +1,37 @@
 package rest
 
-import "io"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
 
 type Reply interface {
 	StatusCode() int
 	Encode(io.Writer) error
+}
+
+type reply struct {
+	value      interface{}
+	statusCode int
+}
+
+func NewReply(value interface{}, statusCode int) Reply {
+	return &reply{
+		value:      value,
+		statusCode: statusCode,
+	}
+}
+
+func NewOKReply(value interface{}) Reply {
+	return NewReply(value, http.StatusOK)
+}
+
+func (r *reply) StatusCode() int {
+	return r.statusCode
+}
+
+func (r *reply) Encode(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(r.value)
 }
